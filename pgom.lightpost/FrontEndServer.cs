508832808467350db1b1ec79735c265ea157c4c8
20chan <2@0chan.dev>
2020-01-05 16:54:49 +0900

    init server commit

---
+++ pgom.lightpost/FrontEndServer.cs
@@ -0,0 +1,31 @@
+﻿using CreamRoll.Routing;
+using System.IO;
+
+namespace pgom.lightpost {
+    public class FrontEndServer {
+        public string DirectoryPath;
+        public string Index;
+
+        public FrontEndServer(string path, string index) {
+            DirectoryPath = path;
+            Index = index;
+        }
+
+        [Get("/{f0}")]
+        [Get("/{f0}/{f1}")]
+        [Get("/{f0}/{f1}/{f2}")]
+        public Response RouteFile(Request req) {
+            var path = req.Uri.AbsolutePath.Substring(1);
+            if (string.IsNullOrEmpty(path)) {
+                path = Index;
+            }
+
+            var fullPath = Path.Combine(DirectoryPath, path);
+            if (!File.Exists(fullPath)) {
+                return new TextResponse("file not found", status: StatusCode.NotFound);
+            }
+
+            return new FileResponse(fullPath);
+        }
+    }
+}
---
+++ pgom.lightpost/Program.cs
@@ -0,0 +1,31 @@
+﻿using CreamRoll.Routing;
+using System;
+using System.Threading;
+
+namespace pgom.lightpost {
+    class Program {
+        static void Main(string[] args) {
+            var post = new LPServer();
+            var frontend = new FrontEndServer("FrontEnd", "index.html");
+
+            var server = new RouteServer<LPServer>(post);
+            server.AppendRoutes(frontend);
+
+            StartDaemon(server);
+        }
+
+        public static void StartDaemon<T>(RouteServer<T> server) {
+            var waiter = new ManualResetEvent(false);
+            Console.CancelKeyPress += (o, e) => {
+                e.Cancel = true;
+                waiter.Set();
+            };
+
+            server.StartAsync();
+            Console.WriteLine("pgom.lightpost started.. press ctrl+c to stop");
+
+            waiter.WaitOne();
+            server.Stop();
+        }
+    }
+}
